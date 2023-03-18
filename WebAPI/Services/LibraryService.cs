59f10e2e59e7a9e5aa285cfd6b677be7815284c4
Alexandros Pournaras <apournaras@york.citycollege.eu>
2023-03-18 11:52:14 +0200

    Convert datalayers into one for both books and authors

--- WebAPI/Services/LibraryService.cs
+++ WebAPI/Services/LibraryService.cs
@@ -3,7 +3,6 @@ using WebAPI.Model;
 
 namespace WebAPI.Services
 {
-    
     public interface ILibraryService
     {
         List<Author> GetAllAuthors();
@@ -23,8 +22,7 @@ namespace WebAPI.Services
 
     public class LibraryService : ILibraryService
     {
-        private AuthorsDatalayer _authorsDatalayer;
-        private BooksDatalayer _booksDatalayer;
+        private LibraryDatalayer _libraryDatalayer;
 
         private static readonly string[] FirstNames = new[]
         {
@@ -53,150 +51,149 @@ namespace WebAPI.Services
 
         public LibraryService()
         {
-            _authorsDatalayer = new AuthorsDatalayer();
-            _booksDatalayer = new BooksDatalayer();
+            _libraryDatalayer = new LibraryDatalayer();
         }
 
         public List<Author> GetAllAuthors()
         {
-            _authorsDatalayer.OpenConnection();
+            _libraryDatalayer.OpenConnection();
 
-            List<Author> authors = _authorsDatalayer.GetAllAuthors();
+            List<Author> authors = _libraryDatalayer.GetAllAuthors();
 
-            _authorsDatalayer.CloseConnection();
+            _libraryDatalayer.CloseConnection();
 
             return authors;
         }
 
         public List<Book> GetAllBooks()
         {
-            _booksDatalayer.OpenConnection();
+            _libraryDatalayer.OpenConnection();
 
-            List<Book> books = _booksDatalayer.GetAllBooks();
+            List<Book> books = _libraryDatalayer.GetAllBooks();
 
-            _booksDatalayer.CloseConnection();
+            _libraryDatalayer.CloseConnection();
 
             return books;
         }
 
         public Author GetAuthorById(int id)
         {
-            _authorsDatalayer.OpenConnection();
+            _libraryDatalayer.OpenConnection();
 
-            Author author = _authorsDatalayer.GetAuthorById(id);
+            Author author = _libraryDatalayer.GetAuthorById(id);
 
-            _authorsDatalayer.CloseConnection();
+            _libraryDatalayer.CloseConnection();
 
             return author;
         }
 
         public Book GetBookById(int id)
         {
-            _booksDatalayer.OpenConnection();
+            _libraryDatalayer.OpenConnection();
 
-            Book book = _booksDatalayer.GetBookById(id);
+            Book book = _libraryDatalayer.GetBookById(id);
 
-            _booksDatalayer.CloseConnection();
+            _libraryDatalayer.CloseConnection();
 
             return book;
         }
 
         public List<Book> GetBooksByAuthor(int authorId)
         {
-            _booksDatalayer.OpenConnection();
+            _libraryDatalayer.OpenConnection();
 
-            List<Book> books = _booksDatalayer.GetBooksByAuthor(authorId);
+            List<Book> books = _libraryDatalayer.GetBooksByAuthor(authorId);
 
-            _booksDatalayer.CloseConnection();
+            _libraryDatalayer.CloseConnection();
 
             return books;
         }
 
         public void AddAuthor(Author author)
         {
-            _authorsDatalayer.OpenConnection();
+            _libraryDatalayer.OpenConnection();
 
-            _authorsDatalayer.AddAuthor(author);
+            _libraryDatalayer.AddAuthor(author);
 
-            _authorsDatalayer.CloseConnection();
+            _libraryDatalayer.CloseConnection();
         }
 
         public void AddBook(Book book)
         {
-            _booksDatalayer.OpenConnection();
+            _libraryDatalayer.OpenConnection();
 
-            _booksDatalayer.AddBook(book);
+            _libraryDatalayer.AddBook(book);
 
-            _booksDatalayer.CloseConnection();
+            _libraryDatalayer.CloseConnection();
         }
 
         public Author UpdateAuthor(int id, Author updatedAuthor)
         {
-            _authorsDatalayer.OpenConnection();
+            _libraryDatalayer.OpenConnection();
 
-            Author existingAuthor = _authorsDatalayer.GetAuthorById(id);
+            Author existingAuthor = _libraryDatalayer.GetAuthorById(id);
 
             if (existingAuthor != null)
             {
-                _authorsDatalayer.UpdateAuthor(existingAuthor, updatedAuthor);
+                _libraryDatalayer.UpdateAuthor(existingAuthor, updatedAuthor);
             }
 
-            _authorsDatalayer.CloseConnection();
+            _libraryDatalayer.CloseConnection();
 
             return existingAuthor;
         }
 
         public Book UpdateBook(int id, Book updatedBook)
         {
-            _booksDatalayer.OpenConnection();
+            _libraryDatalayer.OpenConnection();
 
-            Book existingBook = _booksDatalayer.GetBookById(id);
+            Book existingBook = _libraryDatalayer.GetBookById(id);
 
             if (existingBook != null)
             {
-                _booksDatalayer.UpdateBook(existingBook, updatedBook);
+                _libraryDatalayer.UpdateBook(existingBook, updatedBook);
             }
 
-            _booksDatalayer.CloseConnection();
+            _libraryDatalayer.CloseConnection();
 
             return existingBook;
         }
 
         public Author DeleteAuthor(int id)
         {
-            _authorsDatalayer.OpenConnection();
+            _libraryDatalayer.OpenConnection();
 
-            Author author = _authorsDatalayer.GetAuthorById(id);
+            Author author = _libraryDatalayer.GetAuthorById(id);
 
             if (author != null)
             {
-                _authorsDatalayer.DeleteAuthor(author);
+                _libraryDatalayer.DeleteAuthor(author);
             }
 
-            _authorsDatalayer.CloseConnection();
+            _libraryDatalayer.CloseConnection();
 
             return author;
         }
 
         public Book DeleteBook(int id)
         {
-            _booksDatalayer.OpenConnection();
+            _libraryDatalayer.OpenConnection();
 
-            Book book = _booksDatalayer.GetBookById(id);
+            Book book = _libraryDatalayer.GetBookById(id);
 
             if (book != null)
             {
-                _booksDatalayer.DeleteBook(book);
+                _libraryDatalayer.DeleteBook(book);
             }
 
-            _booksDatalayer.CloseConnection();
+            _libraryDatalayer.CloseConnection();
 
             return book;
         }
 
         public void InitializeAuthors(int numberOfAuthors)
         {
-            _authorsDatalayer.OpenConnection();
+            _libraryDatalayer.OpenConnection();
 
             Random randomizer = new Random();
 
@@ -208,15 +205,15 @@ namespace WebAPI.Services
                 author.LastName = LastNames[randomizer.Next(LastNames.Length)];
                 author.Country = Countries[randomizer.Next(Countries.Length)];
 
-                _authorsDatalayer.AddAuthor(author);
+                _libraryDatalayer.AddAuthor(author);
             }
 
-            _authorsDatalayer.CloseConnection();
+            _libraryDatalayer.CloseConnection();
         }
 
         public void InitializeBooks(int numberOfBooks, List<Author> authors)
         {
-            _booksDatalayer.OpenConnection();
+            _libraryDatalayer.OpenConnection();
 
             Random randomizer = new Random();
 
@@ -234,10 +231,10 @@ namespace WebAPI.Services
                 book.Category = Categories[randomizer.Next(Categories.Length)];
                 book.Pages = randomizer.Next(999);
 
-                _booksDatalayer.AddBook(book);
+                _libraryDatalayer.AddBook(book);
             }
 
-            _booksDatalayer.CloseConnection();
+            _libraryDatalayer.CloseConnection();
         }
     }
 }
